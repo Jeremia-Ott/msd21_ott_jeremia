1a7c68ab9e6db54534c271e202ec30a0ef7c97b5
Jeremia-Ott <jeremia.ott@edu.fh-joanneum.at>
2022-05-15 15:45:20 +0200

    Implement faculty methode

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,19 +1,35 @@
 package at.fhj.msd;
 
 public class Calculator {
-    public double add(double number1, double number2){
+    public double add(double number1, double number2) {
         return number1 + number2;
     }
-    public double minus(double number1, double number2){
+
+    public double minus(double number1, double number2) {
         return number1 - number2;
     }
-    public double divide(double number1, double number2){
+
+    public double divide(double number1, double number2) {
         return number1 / number2;
     }
-    public double multiply(double number1, double number2){
+
+    public double multiply(double number1, double number2) {
         return number1 * number2;
     }
-    public int faculty(int number){
-        return 0;
+
+    public int faculty(int number) {
+        // negative numbers
+        if (number < 0)
+            return 0;
+
+        // Base cases
+        if (number == 0 || number == 1)
+            return 1;
+
+        int result = 1;
+        for (; number > 1; number--){
+            result *= number;
+        }
+        return result;
     }
 }
