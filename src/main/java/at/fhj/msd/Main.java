293be02e1874420b30b3a50132e79a03915ec6d0
Jeremia Ott <jeremia.ott@edu.fh-joanneum.at>
2022-05-18 13:51:04 +0200

    Add Logger + images about the logger

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,6 +1,11 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         double numAdd = calculator.add(6, 2);
@@ -12,5 +17,8 @@ public class Main {
         System.out.println("6 - 2 = " + numMinus);
         System.out.println("6 / 2 = " + numDivide);
         System.out.println("6 * 2 = " + numMultiply);
+
+        logger.info("Hello World");
+        logger.error("foobar");
     }
 }
